2df2bdca59d8a29a0a1b601afca15a22628ce023
Zhuoran He <zhuo1angT@Outlook.com>
2021-01-24 23:15:44 +0800

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,8 +4,13 @@
 export CC=(which clang)
 export CXX=(which clang++)
 
+export GOPATH=/home/zhuo1ang/go
+
 alias tiup="/home/zhuo1ang/.tiup/bin/tiup"
 
 alias setproxy="export https_proxy=https://127.0.0.1:1080;export http_proxy=http://127.0.0.1:1080;export all_proxy=socks5://127.0.0.1:1080;echo \"Set proxy successfully\" "
 alias unsetproxy="set -e http_proxy;set -e https_proxy;set -e all_proxy;echo \"Unset proxy successfully\" "
 
+set -U fish_greeting ""
+
+alias tldr="proxychains tldr"
