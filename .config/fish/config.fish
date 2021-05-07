bb0968b811d8c055ec844b5cbbc7fb9f5601a341
Zhuoran He <zhuo1angT@Outlook.com>
2021-05-07 19:28:21 +0800

    upd

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,8 +8,6 @@ export VISUAL="vim"
 
 export GOPATH=/home/zhuo1ang/go
 
-alias tiup="/home/zhuo1ang/.tiup/bin/tiup"
-
 alias setproxy="export https_proxy=https://127.0.0.1:1080;export http_proxy=http://127.0.0.1:1080;export all_proxy=socks5://127.0.0.1:1080;echo \"Set proxy successfully\" "
 alias unsetproxy="set -e http_proxy;set -e https_proxy;set -e all_proxy;echo \"Unset proxy successfully\" "
 
